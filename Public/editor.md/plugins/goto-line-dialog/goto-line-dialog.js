26d69ed45183aab29c86a6c1924e58e9d0e2571e
star7th <xing7th@gmail.com>
2016-01-06 17:56:31 +0800

    优化编辑器

--- Public/editor.md/plugins/goto-line-dialog/goto-line-dialog.js
+++ Public/editor.md/plugins/goto-line-dialog/goto-line-dialog.js
@@ -77,7 +77,7 @@
 			{			
 				var dialogContent = [
 					"<div class=\"editormd-form\" style=\"padding: 10px 0;\">",
-					"<p style=\"margin: 0;\">" + dialogLang.label + " 1-" + lineCount +"&nbsp;&nbsp;&nbsp;<input type=\"number\" class=\"number-input\" style=\"width: 60px;\" value=\"1\" max=\"" + lineCount + "\" min=\"1\" data-line-number /></p>",
+					"<p style=\"margin: 0;\">" + dialogLang.label + " 1-" + lineCount +"&nbsp;&nbsp;&nbsp;<input type=\"number\" class=\"number-input\" style=\"width: 60px;height: 30px;\" value=\"1\" max=\"" + lineCount + "\" min=\"1\" data-line-number /></p>",
 					"</div>"
 				].join("\n");
 
