f116a16289edf6bc73cd049c8b1942ca25923b22
star7th <xing7th@gmail.com>
2017-10-31 16:05:18 +0800

    Update index.js

--- Public/js/page/index.js
+++ Public/js/page/index.js
@@ -3,7 +3,7 @@ $(function(){
     hljs.initHighlightingOnLoad();
     
     var EditormdView = editormd.markdownToHTML("page_md_content", {
-      htmlDecode      : "style,script,iframe",  // you can filter tags decode
+      htmlDecode      : false,  // you can filter tags decode
       emoji           : false,
       taskList        : true,
       tex             : true,  // 默认不解析
