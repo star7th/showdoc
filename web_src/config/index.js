bb239de4f54c4414cffd8e05c750d3398c8339d7
star7th <xing7th@gmail.com>
2018-09-02 10:56:46 +0800

    dev

--- web_src/config/index.js
+++ web_src/config/index.js
@@ -10,7 +10,9 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+        '/server/': 'http://127.0.0.1/showdoc/'
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
