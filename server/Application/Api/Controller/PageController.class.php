dfe1a5483f146f6794e194f0287b885738d9c667
star7th <xing7th@gmail.com>
2019-04-01 15:00:41 +0800

    Update PageController.class.php

--- server/Application/Api/Controller/PageController.class.php
+++ server/Application/Api/Controller/PageController.class.php
@@ -226,7 +226,7 @@ class PageController extends BaseController {
           }
         }else{
             $upload = new \Think\Upload();// 实例化上传类
-            $upload->maxSize  = 3145728 ;// 设置附件上传大小
+            $upload->maxSize  = 1003145728 ;// 设置附件上传大小
             $upload->allowExts  = array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
             $upload->rootPath = './../Public/Uploads/';// 设置附件上传目录
             $upload->savePath = '';// 设置附件上传子目录
