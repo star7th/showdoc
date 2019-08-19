c21df36a5132b8cbf47d0fb26bfd740bccf94ea0
star7th <xing7th@gmail.com>
2019-08-19 20:22:29 +0800

    reset admin's password 新增重置管理员密码功能

--- server/Application/Home/Controller/CommonController.class.php
+++ server/Application/Home/Controller/CommonController.class.php
@@ -29,4 +29,24 @@ class CommonController extends BaseController {
 
     }
 
+
+    //重置管理员用户密码
+    //使用方式：
+    //若用官方自动脚本或者docker方式安装，则需在命令行里执行docker exec showdoc php  /var/www/html/index.php home/common/repasswd
+    //若是手动安装php环境的，则在命令行中切换到showdoc目录，执行php index.php home/common/repasswd
+    //执行后会把管理员用户showdoc的密码重置为123456
+    public function repasswd(){
+    if (preg_match("/cli/i", php_sapi_name()) ) {
+        if(D("User")->where("username = 'showdoc' ")->find()){
+            D("User")->where("username = 'showdoc' ")->save(array("groupid"=> 1,'password'=>"a89da13684490eb9ec9e613f91d24d00" )) ;
+        }else{
+             D("User")->add(array('username'=>"showdoc" ,"groupid"=>1,'password'=>"a89da13684490eb9ec9e613f91d24d00" , 'reg_time'=>time()));
+        }
+        echo "ok \n" ;
+    }else{
+        echo "please run in command line";
+    }
+
+    }
+
 }
