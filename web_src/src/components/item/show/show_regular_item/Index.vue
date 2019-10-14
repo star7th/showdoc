8375eea0ef12d71af11799f208aefc7611765561
star7th <xing7th@gmail.com>
2019-10-14 18:04:53 +0800

    Update Index.vue

--- web_src/src/components/item/show/show_regular_item/Index.vue
+++ web_src/src/components/item/show/show_regular_item/Index.vue
@@ -19,6 +19,7 @@
             <div class="header-right">
               <!-- 登录的事情下 -->
                <router-link v-if="item_info.is_login" to="/item/index" >{{$t('goback')}} </router-link>
+                &nbsp;&nbsp;&nbsp;&nbsp;
               <el-dropdown @command="dropdown_callback" v-if="item_info.is_login">
                 <span class="el-dropdown-link">
                   {{$t('item')}}<i class="el-icon-arrow-down el-icon--right"></i>
