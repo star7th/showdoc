fa372e31bde36d0c0ef8d525116a8cbc9040646b
star7th <xing7th@gmail.com>
2020-02-16 23:09:54 +0800

    time

--- server/Application/Api/Controller/UserController.class.php
+++ server/Application/Api/Controller/UserController.class.php
@@ -71,8 +71,8 @@ class UserController extends BaseController {
           unset($ret['password']);
           session("login_user" , $ret );
           D("User")->setLastTime($ret['uid']);
-          $token = D("UserToken")->createToken($ret['uid']);
-          cookie('cookie_token',$token,array('expire'=>60*60*24*90,'httponly'=>'httponly'));//此处由服务端控制token是否过期，所以cookies过期时间设置多久都无所谓
+          $token = D("UserToken")->createToken($ret['uid'],60*60*24*180);
+          cookie('cookie_token',$token,array('expire'=>60*60*24*180,'httponly'=>'httponly'));//此处由服务端控制token是否过期，所以cookies过期时间设置多久都无所谓
           $this->sendResult(array());               
         }else{
             D("VerifyCode")->_ins_times($key);//输错密码则设置输错次数
@@ -109,7 +109,7 @@ class UserController extends BaseController {
           unset($ret['password']);
           session("login_user" , $ret );
           D("User")->setLastTime($ret['uid']);
-          $token = D("UserToken")->createToken($ret['uid']);
+          $token = D("UserToken")->createToken($ret['uid'], 60*60*24*180);
           $this->sendResult(array(
             "uid" => $ret['uid'] ,
             "username" => $ret['username'] ,
