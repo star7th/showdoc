7b42d4b6bda7a04942473b3042df0a96ad729067
star7th <xing7th@gmai.com>
2020-12-29 14:14:13 +0800

    New home page / 新首页

--- web_src/src/components/Index.vue
+++ web_src/src/components/Index.vue
@@ -1,106 +1,183 @@
 <template>
-  <div class="hello">
-    <div class="block">
-      <div class="row header">
-        <div class="right pull-right">
-          <ul class="inline pull-right">
+  <div class="body">
+    <!--header-->
+    <div class="header">
+      <div class="header-wrap">
+        <div class="logo">
+          <a href="/">
+            <img src="static/imgs/Logo.png" />
+          </a>
+        </div>
+        <input type="checkbox" name id="mobile-menu-toggle" value />
+        <label class="gh" for="mobile-menu-toggle">
+          <span></span>
+        </label>
+        <div class="nav">
+          <ul>
             <li>
-              <router-link :to="link">{{link_text}}</router-link>&nbsp;&nbsp;&nbsp;
-              <a
-                target="_blank"
-                v-if="lang =='zh-cn'"
-                href="https://www.showdoc.cc/clients"
-              >客户端</a>
+              <router-link :to="link">{{link_text}}</router-link>
+            </li>
+            <li>
+              <a target="_blank" v-if="lang =='zh-cn'" href="https://www.showdoc.cc/clients">客户端</a>
             </li>
           </ul>
         </div>
       </div>
+    </div>
 
-      <el-carousel :height="height" :autoplay="false" arrow="always">
-        <el-carousel-item style="background-color: #1bbc9b;">
-          <div class="slide">
-            <img src="static/logo/b_64.png" alt />
-            <h2>{{$t("section_title1")}}</h2>
+    <div class="hbanner">
+      <div class="wrapper">
+        <div class="hbanner-txt">
+          <h2>
+            {{$t('section_description1_1')}}
+            <br />
+            <font class="f-blue">{{$t('section_description1_2')}}</font>
+          </h2>
+          <div class="btns">
+            <a href="https://www.showdoc.cc/demo" target="_blank" class="btn on">{{$t("demo")}}</a>
+            <a href="https://www.showdoc.cc/help" target="_blank" class="btn">{{$t("more")}}</a>
+          </div>
+        </div>
+        <div class="hbanner-imgs"></div>
+      </div>
+    </div>
+    <div class="hrow hrow1">
+      <div class="wrapper">
+        <div class="img fl">
+          <img src="static/imgs/home2.png" />
+          <div class="box">
+            <img src="static/imgs/home2-img.png" />
+          </div>
+        </div>
+        <div class="txt fr">
+          <h2>{{$t("section_title2")}}</h2>
+          <div class="desc">
             <p>
-              <span v-html="$t('section_description1')"></span>
+              <img src="static/imgs/Vector.png" />
+              {{$t("section_description2_1")}}
             </p>
             <p>
-              <a class="el-button" href="https://www.showdoc.cc/demo" target="_blank">{{$t("demo")}}</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <a
-                class="el-button"
-                href="https://www.showdoc.cc/help"
-                target="_blank"
-              >{{$t("help")}}&nbsp;</a>
+              <img src="static/imgs/Vector.png" />
+              {{$t("section_description2_2")}}
             </p>
           </div>
-        </el-carousel-item>
+        </div>
+      </div>
+    </div>
 
-        <el-carousel-item style="background-color: #2C606A;">
-          <div class="slide">
-            <h2>{{$t("section_title2")}}</h2>
+    <div class="hrow hrow2">
+      <div class="wrapper">
+        <div class="img fr">
+          <img src="static/imgs/home3.png" />
+        </div>
+        <div class="txt fl">
+          <h2>{{$t("section_title3")}}</h2>
+          <div class="desc">
             <p>
-              <span v-html="$t('section_description2')"></span>
+              <img src="static/imgs/Vector1.png" />
+              {{$t("section_description3_1")}}
+            </p>
+            <p>
+              <img src="static/imgs/Vector1.png" />
+              {{$t("section_description3_2")}}
             </p>
           </div>
-        </el-carousel-item>
+        </div>
+      </div>
+    </div>
 
-        <el-carousel-item style="background-color: #f90;">
-          <div class="slide">
-            <h2>{{$t("section_title3")}}</h2>
+    <div class="hrow hrow3">
+      <div class="wrapper">
+        <div class="img fl">
+          <img src="static/imgs/home4.png" />
+        </div>
+        <div class="txt fr">
+          <h2>{{$t("section_title4")}}</h2>
+          <div class="desc">
             <p>
-              <span v-html="$t('section_description3')"></span>
+              <img src="static/imgs/Vector.png" />
+              {{$t("section_description4_1")}}
+            </p>
+            <p>
+              <img src="static/imgs/Vector.png" />
+              {{$t("section_description4_2")}}
             </p>
           </div>
-        </el-carousel-item>
+        </div>
+      </div>
+    </div>
 
-        <el-carousel-item style="background-color: #7CBD9D;">
-          <div class="slide">
-            <h2>{{$t("section_title4")}}</h2>
+    <div class="hrow hrow4">
+      <div class="wrapper">
+        <div class="img fr">
+          <img src="static/imgs/home5.png" />
+        </div>
+        <div class="txt fl">
+          <h2>{{$t("section_title5")}}</h2>
+          <div class="desc">
+            <p>{{$t("section_description5")}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="hrow hrow5">
+      <div class="wrapper">
+        <div class="img fl">
+          <img src="static/imgs/home6.png" />
+        </div>
+        <div class="txt fr">
+          <h2>{{$t("section_title6")}}</h2>
+          <div class="desc">
             <p>
-              <span v-html="$t('section_description4')"></span>
+              <img src="static/imgs/Vector1.png" />
+              {{$t("section_description6_1")}}
+            </p>
+            <p>
+              <img src="static/imgs/Vector1.png" />
+              {{$t("section_description6_2")}}
             </p>
           </div>
-        </el-carousel-item>
-
-        <el-carousel-item style="background-color: #A77DC2;">
-          <div class="slide">
-            <h2>{{$t("section_title5")}}</h2>
-            <p>
-              <span v-html="$t('section_description5')"></span>
-            </p>
+        </div>
+      </div>
+    </div>
+    <div class="hrow hrow6">
+      <div class="wrapper">
+        <div class="img fr">
+          <img src="static/imgs/home7.png" />
+        </div>
+        <div class="txt fl">
+          <h2>{{$t("section_title7")}}</h2>
+          <div class="desc">
+            <p>{{$t("section_description7_1")}}</p>
+            <p>{{$t("section_description7_2")}}</p>
           </div>
-        </el-carousel-item>
+        </div>
+      </div>
+    </div>
 
-        <el-carousel-item style="background-color: #85CE92;">
-          <div class="slide">
-            <h2>{{$t("section_title6")}}</h2>
-            <p>
-              <span v-html="$t('section_description6')"></span>
-            </p>
+    <div class="hrow hrow7">
+      <div class="wrapper">
+        <div class="img fl">
+          <img src="static/imgs/home8.png" />
+        </div>
+        <div class="txt fr">
+          <h2>{{$t("section_title8")}}</h2>
+          <div class="desc">
+            <p>{{$t("section_description8_1")}}</p>
+            <p>{{$t("section_description8_2")}}</p>
           </div>
-        </el-carousel-item>
-
-        <el-carousel-item style="background-color: #4BBFC3;">
-          <div class="slide">
-            <h2>{{$t("section_title7")}}</h2>
-            <p>
-              <span v-html="$t('section_description7')"></span>
-            </p>
-          </div>
-        </el-carousel-item>
-
-        <el-carousel-item style="background-color: #1bbc9b;">
-          <div class="slide">
-            <h2></h2>
-            <p>{{$t("section_description8")}}</p>
-            <p>
-              <el-button>
-                <router-link to="/user/login">{{$t("section_title8")}}</router-link>
-              </el-button>
-            </p>
-          </div>
-        </el-carousel-item>
-      </el-carousel>
+        </div>
+      </div>
+    </div>
+    <div class="hfoot">
+      <div class="wrapper">
+        <h2>{{$t("section_description9")}}</h2>
+        <router-link to="/user/login" class="btn">{{$t("section_title9")}}</router-link>
+      </div>
+      <div class="copyright">
+        <a href="http://www.beian.miit.gov.cn/"></a>
+      </div>
     </div>
   </div>
 </template>
@@ -167,45 +244,6 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.el-carousel__item {
-  text-align: center;
-  font: 25px 'Microsoft Yahei';
-  color: #fff;
-}
-
-.header {
-  padding-right: 50px;
-  padding-top: 30px;
-  font-size: 18px;
-  position: fixed;
-  right: 0;
-  left: 0;
-  z-index: 1030;
-  margin-bottom: 0;
-}
-.header a {
-  color: white;
-  font-size: 12px;
-  font-weight: bold;
-}
-.slide {
-  width: 100%;
-  max-width: 700px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  padding-top: 0px;
-  padding-left: 15px;
-  padding-right: 15px;
-  padding-bottom: 0px;
-  box-sizing: border-box;
-}
-
-@media only screen and (max-width: 800px) {
-  .slide p {
-    font-size: 14px;
-  }
-}
-</style>
+<style scoped src="@/../static/css/qietu.css"></style>
+<style scoped src="@/../static/css/style.css"></style>
+<style scoped src="@/../static/css/responsive.css"></style>
