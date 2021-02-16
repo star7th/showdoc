91c4dd1f1a99cd3a7b71ecbaf2a7ad142ae9fb0c
123 <123@456.123>
2021-02-16 23:38:04 +0800

    修复复制代码功能bug，样式修改

---
+++ web_src/build.bat
@@ -0,0 +1,2 @@
+npm run build
+pause
--- web_src/src/components/item/show/show_regular_item/Toc.vue
+++ web_src/src/components/item/show/show_regular_item/Toc.vue
@@ -96,7 +96,7 @@ export default {
     html_encode(str){
       var s = "";
       if (str.length == 0) return "";
-      s = str.replace(/&/g, "&gt;");
+      s = str.replace(/&/g, "&amp;");
       s = s.replace(/</g, "&lt;");
       s = s.replace(/>/g, "&gt;");
       //s = s.replace(/ /g, "&nbsp;");
