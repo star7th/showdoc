89542852953e9b993f2b813a77497915b411a8ef
star7th <xing7th@gmai.com>
2021-04-21 01:10:08 +0800

    composer

---
+++ server/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'v2.8.14',
+    'version' => '2.8.14.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'showdoc/showdoc',
+  ),
+  'versions' => 
+  array (
+    'showdoc/showdoc' => 
+    array (
+      'pretty_version' => 'v2.8.14',
+      'version' => '2.8.14.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
