c97f3b447587e69559679ff7b75d208f4ebd6900
star7th <xing7th@gmail.com>
2021-05-31 20:34:55 +0800

    autoload

--- server/index.php
+++ server/index.php
@@ -1,21 +1,12 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006-2014 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
-// +----------------------------------------------------------------------
 
-// 增加自动加载
-require './vendor/autoload.php';
-
-// 应用入口文件
-
-// 检测PHP环境
-if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+// 当php版本达到composer包要求的版本时候，则require autoload
+// vendor/composer/platform_check.php 里面可以看到composer包要求的最低版本
+define('COMPOSER_PHP_VERSION', '7.2.5' );
+if(version_compare(PHP_VERSION,COMPOSER_PHP_VERSION,'>')){
+    // 增加自动加载
+    require './vendor/autoload.php';
+}
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG',True);
@@ -25,5 +16,3 @@ define('APP_PATH','./Application/');
 
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
-
-// 亲^_^ 后面不需要任何代码了 就是如此简单
