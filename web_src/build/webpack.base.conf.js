620ac8dcdea5e0de17af913e530ceb0c6e8d421a
star7th <xing7th@gmail.com>
2022-12-20 11:34:08 +0800

    update

--- web_src/build/webpack.base.conf.js
+++ web_src/build/webpack.base.conf.js
@@ -3,8 +3,8 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
-require("babel-polyfill");
-function resolve (dir) {
+require('babel-polyfill')
+function resolve(dir) {
   return path.join(__dirname, '..', dir)
 }
 
@@ -27,15 +27,16 @@ module.exports = {
   output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
-    publicPath: process.env.NODE_ENV === 'production'
-      ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
+    publicPath:
+      process.env.NODE_ENV === 'production'
+        ? config.build.assetsPublicPath
+        : config.dev.assetsPublicPath
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
-      'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
+      vue$: 'vue/dist/vue.esm.js',
+      '@': resolve('src')
     }
   },
   module: {
@@ -74,6 +75,10 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
+      },
+      {
+        test: /.sass$/,
+        loaders: ['style', 'css', 'sass']
       }
     ]
   },
