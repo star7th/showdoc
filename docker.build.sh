95ebb92f8350c5380aa3d0c0db84e6061f95a31e
xiagw <fxiaxiaoyu@gmail.com>
2023-07-14 17:34:11 +0800

    fix remove backup files

--- docker.build.sh
+++ docker.build.sh
@@ -4,24 +4,26 @@ set -xe
 
 rm -rf /app
 ln -s /var/www/html /app
-echo "upload_max_filesize=5000M;" >>/opt/docker/etc/php/php.ini
-echo "post_max_size=5000M;" >>/opt/docker/etc/php/php.ini
-echo "client_max_body_size 5000m;" >/opt/docker/etc/nginx/vhost.common.d/10-general.conf
-## TODO: fix warning
-# /opt/docker/etc/nginx/vhost.conf
-# /opt/docker/etc/nginx/vhost.ssl.conf
-#
-
-mv /showdoc_data/html/mock /showdoc_data/
+(
+    echo "upload_max_filesize=5120M;"
+    echo "post_max_size=5120M;"
+) >>/opt/docker/etc/php/php.ini
+echo "client_max_body_size 5120m;" >/opt/docker/etc/nginx/vhost.common.d/10-general.conf
+## fix nginx warning
+sed -i -e '1d' /opt/docker/etc/nginx/vhost.ssl.conf
+sed -i -e '/443\ default_server/s//443\ default_server\ ssl/' /opt/docker/etc/nginx/vhost.conf
 
 if [ "$IN_CHINA" = true ] && [ -f /etc/apk/repositories ]; then
     sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/' /etc/apk/repositories
 fi
 apk update
-apk add --update nodejs npm
+apk add --update --no-cache nodejs npm
 
+mv /showdoc_data/html/mock /showdoc_data/
 cd /showdoc_data/mock || exit 1
 if [ "$IN_CHINA" = true ]; then
     npm config set registry https://registry.npmmirror.com/
 fi
+## fix old warn
+rm -f package-lock.json
 npm install
--- docker.run.sh
+++ docker.run.sh
@@ -9,10 +9,11 @@ fi
 ## backup sqlite file every day
 db_file=/var/www/html/Sqlite/showdoc.db.php
 while [ -f $db_file ]; do
-    if [[ $(date +%H%M) == 0401 ]]; then
+    # backup on 20:01 (UTC) every day
+    if [[ $(date +%H%M) == 2001 ]]; then
         \cp $db_file ${db_file}."$(date +%F-%H-%M-%S)".php
         ## remove old files (15 days ago)
-        find ${db_file}* -type f -ctime +15 -print0 |
+        find ${db_file}.* -type f -ctime +15 -print0 |
             xargs -t -0 rm -f >/dev/null
     fi
     sleep 50
@@ -29,6 +30,6 @@ chmod 777 -R /var/www/
     sleep 30
     cd /showdoc_data/mock/
     npm run start
-)
+) &
 
 supervisord
