07aa90a51408ba50c0a16974dedb87a542d18d97
star7th <xing7th@gmail.com>
2023-12-21 20:56:08 +0800

    update KaTeX

--- web_src/static/editor.md/editormd.js
+++ web_src/static/editor.md/editormd.js
@@ -3931,8 +3931,8 @@
   // 使用国外的CDN，加载速度有时会很慢，或者自定义URL
   // You can custom KaTeX load url.
   editormd.katexURL = {
-    css: '//cdn.staticfile.org/KaTeX/0.3.0/katex.min',
-    js: '//cdn.staticfile.org/KaTeX/0.3.0/katex.min'
+    css: '//cdn.staticfile.org/KaTeX/0.12.0/katex.min',
+    js: '//cdn.staticfile.org/KaTeX/0.12.0/katex.min'
   }
 
   editormd.kaTeXLoaded = false
