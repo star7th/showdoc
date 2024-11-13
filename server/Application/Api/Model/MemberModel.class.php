4107347f610d4b954cd609161b91884cf4a24ad0
star7th <xing7th@gmail.com>
2024-11-14 03:52:40 +0800

    update

--- server/Application/Api/Model/MemberModel.class.php
+++ server/Application/Api/Model/MemberModel.class.php
@@ -14,7 +14,13 @@ class MemberModel extends BaseModel
         $cat_id1 = D("ItemMember")->where(" item_id = '%d' and uid = '%d' ", array($item_id, $uid))->getField('cat_id');
         $cat_id2 = D("TeamItemMember")->where(" item_id = '%d' and member_uid = '%d' ", array($item_id, $uid))->getField('cat_id');
         // 尝试给 $cat_id 赋值 $cat_id1，如果 $cat_id1 未空，则尝试 $cat_id2，如果 $cat_id2 也未空，则最终给 $cat_id 赋值 0。
-        $cat_id = $cat_id1 ?? $cat_id2 ?? 0;
+        if (!empty($cat_id1)) {
+            $cat_id = $cat_id1;
+        } elseif (!empty($cat_id2)) {
+            $cat_id = $cat_id2;
+        } else {
+            $cat_id = 0;
+        }
         return $cat_id;
     }
 }
